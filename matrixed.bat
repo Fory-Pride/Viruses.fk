9635fbde036b3aef678f767cf670c80d8cb0e49f
Fory Pride <qtaleproyt@gmail.com>
2025-04-25 18:55:58 +0200

    Add files via upload

---
+++ matrixed.bat
@@ -0,0 +1,6 @@
+speed 0.1
+color 4
+@echo off
+:loop 
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto loop
